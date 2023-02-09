b7ee696bbf01135aaff5669b09e6da8a55df9662
vietdata <vis249x9@gmail.com>
2023-02-10 00:00:49 +0700

    new process

--- models/preprocess.py
+++ models/preprocess.py
@@ -11,29 +11,34 @@ class EstateData:
         self.path = path
         self.data = pd.read_csv(path)
         self.districts = self.data["District"][self.data.Area.notna()]
-        self.data = self.data[['House Direction', 'Balcony Direction', 'Toilets','Bedrooms', 
-           'Legits', 'Floors', 'Facade', 'Entrance', "Area", 'X', 'Y',
-            'Price']]
         self.data = self.data[self.data.Area.notna()]
+        self.data["PricePerM2"] = self.data["Price"]/self.data["Area"]
+        self.data = self.data[['House Direction', 'Balcony Direction', 'Toilets','Bedrooms', 
+           'Legits', 'Floors', 'Facade', 'Entrance', 'X', 'Y',
+            "PricePerM2"]]
+        
         cates = ["House Direction", "Balcony Direction",  "Toilets", "Legits", "Floors", "Bedrooms"]
         for f in cates:
              self.data[f] = self.data[f].astype("category")
+        
         self.train = None
         self.test = None
         
     def split_data(self, test_size:int=0.2, random_state:int=0, stratify=True):
         if stratify:
-            train, test = train_test_split(self.data, test_size = test_size, stratify=self.districts)
+            train, test = train_test_split(self.data, test_size = test_size, stratify=self.districts, random_state=random_state)
         else:
             train, test = train_test_split(self.data, test_size = test_size, random_state=random_state)
         self.train = train
         self.test = test
         
-    def tukey_fence(self, series):
+    def tukey_fence(self, series, target=None):
         Q1 = series.quantile(0.25)
         Q3 = series.quantile(0.75)
         IQR = Q3 - Q1
         mask = ~((series < (Q1 - 1.5 * IQR)) |(series > (Q3 + 1.5 * IQR)))
+        if target is not None:
+            mask = ~((target < (Q1 - 1.5 * IQR)) |(target > (Q3 + 1.5 * IQR)))
         return mask
     
     def hard_fence(self, series):
@@ -44,11 +49,13 @@ class EstateData:
         data = self.train
         #fill missing values
         #amean = data["Area"].mean()
+        old_entrance = data["Entrance"][data["Entrance"].notna()]
+        old_facade = data["Facade"][data["Facade"].notna()]
         print("Start Process!")
-        fmean = data["Facade"].mean()
-        emean = data["Entrance"].mean()
+        fmean = data["Facade"][data["Facade"].notna()].mean()
+        emean = data["Entrance"][data["Entrance"].notna()].mean()
         #data["Area"].fillna(value=data["Area"].mean(), inplace=True)
-        data["Facade"].fillna(value=data["Facade"].mean(), inplace=True)
+        data["Facade"].fillna(value=fmean, inplace=True)
         data["Entrance"].fillna(value=emean, inplace=True)
         
         #self.test["Area"].fillna(value=amean, inplace=True)
@@ -57,25 +64,24 @@ class EstateData:
         
         print("Fill missing values: Done")
         
-        data["PricePerM2"] = data["Price"]/data["Area"]
+        #data["PricePerM2"] = data["Price"]/data["Area"]
         data = data[data["PricePerM2"] <= 5000]
         data = data[data["PricePerM2"] >= 10]
-        self.test["PricePerM2"] = self.test["Price"]/self.test["Area"]
+        #self.test["PricePerM2"] = self.test["Price"]/self.test["Area"]
         self.test = self.test[self.test["PricePerM2"]<=5000]
         self.test = self.test[self.test["PricePerM2"]>=10]
-        
         #remove outlier
         if tukey:
-            mask = self.tukey_fence(data["Area"])
-            mask = mask & self.tukey_fence(data["Price"])
-            #mask = mask & self.tukey_fence(data["Entrance"])
-            mask = mask & self.tukey_fence(data["Area"])
+            #mask = self.tukey_fence(data["Area"])
+            mask = self.tukey_fence(data["PricePerM2"])
+            mask = mask & self.tukey_fence(old_entrance, data["Entrance"])
+            mask = mask & self.tukey_fence(old_facade, data["Facade"])
             data = data[mask]
         else:
-            mask = self.hard_fence(data["Area"])
-            mask = mask & self.hard_fence(data["Price"])
-            #mask = mask & self.hard_fence(data["Entrance"])
-            mask = mask & self.hard_fence(data["Area"])
+            #mask = self.hard_fence(data["Area"])
+            mask = self.hard_fence(data["PricePerM2"])
+            mask = mask & self.hard_fence(old_entrance, data["Entrance"])
+            mask = mask & self.hard_fence(old_facade, data["Facade"])
             data = data[mask]
         print("Remove outlier: Done")
         #scale features
@@ -85,7 +91,7 @@ class EstateData:
         self.test[features_] = scaler.transform(self.test[features_])
         
         scaler = StandardScaler()
-        features_ = ['X', 'Y', "Area"]
+        features_ = ['X', 'Y']
         data[features_] = scaler.fit_transform(data[features_])
         self.test[features_] = scaler.transform(self.test[features_])
         print("Scale features: Done")
